a5c22f1cbeb75182547cf88070b1b62dd573d556
TheMisterRedFox <rougefox85@gmail.com>
2022-02-02 14:33:46 +0100

    add searching pokemon by id

--- script/pokemons.js
+++ script/pokemons.js
@@ -6,10 +6,10 @@ const colors = {
   ground: "#f4e7da",
   rock: "#d5d5d4",
   fairy: "#fceaff",
-  poison: "#98d7a5",
+  poison: "#c098d777", //"#98d7a5",//
   bug: "#f8d5a3",
   dragon: "#97b3e6",
-  psychic: "#eaeda1",
+  psychic: "#c50d724b", //"#eaeda1",//
   flying: "#F5F5F5",
   fighting: "#E6E0D4",
   normal: "#F5F5F5",
--- script/script.js
+++ script/script.js
@@ -2,20 +2,50 @@ const searchPokemonById = (id) => {
   for (let i = 0; i < pokemons.length; i++) {
     if (pokemons[i].id === id) {
       console.log(pokemons[i]);
-      return pokemon[i];
+      return pokemons[i];
     }
   }
 };
 
+const uppercaseFirstLetter = (pokemonName) => {
+  return pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+};
+
+const displayPokmon = (pokemon) => {
+  const pokemonPicture = document.querySelector("#pokemon-img");
+  pokemonPicture.src = pokemon.picture;
+
+  const pokemonId = document.querySelector("#pokemon-id");
+  pokemonId.innerText = "#" + pokemon.id.toString().padStart(3, "0");
+
+  const pokemonName = document.querySelector("#pokemon-name");
+  pokemonName.innerText = uppercaseFirstLetter(pokemon.name);
+
+  const pokemonHeight = document.querySelector("#pokemon-height span");
+  pokemonHeight.innerText = pokemon.height / 10 + "m";
+
+  const pokemonWeight = document.querySelector("#pokemon-weight span");
+  pokemonWeight.innerText = pokemon.weight / 10 + "kg";
+
+  const screen = document.querySelector("#screen");
+  const backgroundColor = colors[pokemon.type.name];
+  screen.style = `background-color: ${backgroundColor}`;
+};
+
 const handleNumberSearch = (event) => {
-  event.preventDefault();
   const searchedId = parseInt(document.querySelector("#search-input").value);
   const pokemon = searchPokemonById(searchedId);
+  displayPokmon(pokemon);
+};
+
+const noSubmitProblem = (event) => {
+  event.preventDefault();
 };
 
 const main = () => {
   const form = document.querySelector("form");
-  form.addEventListener("submit", handleNumberSearch);
+  form.addEventListener("input", handleNumberSearch);
+  form.addEventListener("submit", noSubmitProblem);
 
   const pokeball = document.querySelector("form img");
   pokeball.addEventListener("click", handleNumberSearch);
